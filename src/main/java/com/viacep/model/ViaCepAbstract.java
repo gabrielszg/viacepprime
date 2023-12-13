f62a7f9170d62b1def7d28784aa15fd578c13265
gabrielszg <gabrielsouza5@hotmail.com>
2023-12-13 14:38:48 -0300

    refactor: configuration file webservice

--- src/main/java/com/viacep/model/ViaCepAbstract.java
+++ src/main/java/com/viacep/model/ViaCepAbstract.java
@@ -2,6 +2,8 @@ package com.viacep.model;
 
 import java.io.Serializable;
 
+import com.viacep.configs.PropertiesReader;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,8 +12,9 @@ import lombok.Setter;
 public abstract class ViaCepAbstract implements Serializable {
 
 	protected static final long serialVersionUID = 1L;
-
-	protected final String WEBSERVICE = "http://viacep.com.br/ws/";
+	
+	protected final String WEBSERVICE = PropertiesReader.getProperties().getProperty("viacep");
+//	protected final String WEBSERVICE = "http://viacep.com.br/ws/";
 	protected Integer zipCode;
 	protected String federalUnit;
 	protected String city;
